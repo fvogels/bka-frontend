b8f2a58842b1c2d288e6c7412ae3987212755c30
Frederic Vogels <frederic.vogels@gmail.com>
2026-02-15 22:17:18 +0100

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,51 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { viteSingleFile } from "vite-plugin-singlefile"
+
+interface Definitions
+{
+    ROOT_URL : string;
+}
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({command}) => {
+    const isDevBuild = command === 'serve';
+
+    return {
+        plugins: [
+            react(),
+            tsconfigPaths(),
+            viteSingleFile(),
+        ],
+        define: buildSpecificationDefinitions(),
+        ...proxySettings(),
+    };
+
+    function proxySettings() : object
+    {
+        if ( isDevBuild )
+            {
+            return {
+                server: {
+                    proxy: {
+                        '/api/v1': {
+                            target: 'http://localhost:8000'
+                        }
+                    }
+                }
+            };
+        }
+        else
+            {
+            return {};
+        }
+    }
+
+    function buildSpecificationDefinitions() : Definitions
+    {
+        return {
+            ROOT_URL: JSON.stringify('/api/v1'),
+        };
+    }
 })
