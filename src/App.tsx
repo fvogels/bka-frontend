ebcac1e01088ad3d6b7bedb7d328b5c156dfce1b
Frederic Vogels <frederic.vogels@gmail.com>
2026-02-15 18:32:02 +0100

    initial version

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+
+function App() {
+  return (
+    <MantineProvider>
+      <p>Hello world!!</p>
+    </MantineProvider>
+  );
+}
+
+export default App
