05701525d032c8089e62434281aaec648b989856
Frederic Vogels <frederic.vogels@gmail.com>
2026-02-16 13:28:34 +0100

    changed style imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
-import '@mantine/core/styles.layer.css';
-import 'mantine-datatable/styles.layer.css';
-import './layout.css';
+import 'mantine-datatable/styles.css';
+// import '@mantine/core/styles.layer.css';
+// import 'mantine-datatable/styles.layer.css';
+// import './layout.css';
 import { Notifications } from "@mantine/notifications";
 import SearchScreen from './components/SearchScreen';
 
